182a9e0f87d769a76e5192a2cf11bf1a169a0b52
Darakshan2025 <dmaxm2024@gmail.com>
2025-03-25 14:42:16 -1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import json
+
+# load & save the library data
+   
+def load_library():
+    try:
+        with open("library.json", "r") as file:
+            return json.load(file)   
+    except FileNotFoundError :
+         return[]      
+    
+
+def save_library():   
+    with open("library.json", "w") as file:
+        json.dump(library, file, indent=4)
+
+ # initialize library       
+library = load_library()
+
+st.title("Personal Library Manager")
+menu = st.sidebar.radio("select an option", ["View Library", "Add Book", "Remove Book", "Search Book", "Save & Exit"])
+if menu == "View Library":
+    st.sidebar.title("Your Library")
+    if library:
+        st.table(library)
+    else:
+        st.write("No Book available in your library. Add some books!")
+
+# Add book
+elif menu == "Add Book":
+    st.sidebar.title("Add a new book")
+    title = st.text_input("Title")
+    author = st.text_input("Author")
+    year = st.number_input("Year",min_value=2022, max_value=2100,step=1)
+    genre = st.text_input("Genre")
+    read_status = st.checkbox("Mark as Read")
+
+    if st.button("Add Book"):
+        library.append({"title":title,"author":author,"year":year, "genre":genre,"read_status":read_status}) 
+        save_library()
+        st.success("Book added successfully!")
+        st.rerun()                    
+
+#Remove Book
+elif menu == "Remove Book":
+    st.sidebar("Remove a book")
+    book_titles = [book["title"] for book in library]  
+
+    if book_titles:
+        selected_book =  st.selectbox("Select a Book to remove", book_titles)
+        if st.button("Remove book"):
+            library = [book for book in library if book ["title"] != selected_book] 
+            save_library()
+            st.success("Book removed successfully!")
+            st.rerun()
+        else:
+            st.warning("N0 book in your library. Add some books!")
+
+ # search book
+elif menu == "Search Book":
+    st.sidebar("Search a book")
+    search_term = st.text_input("Enter a title or author name")
+    
+    if st.button("Search"):
+        results = [book for book in library if search_term.lower() in book["title"].lower() or search_term.lower() in book["author"].lower()]
+        if results:
+            st.table(results)
+        else:
+            st.warning("No book found!")
+
+ # save and exit 
+elif menu == "Save & Exit":
+    save_library()
+    st.success("Library Saved sucessfully !")           
+             
